8b6574949562b0464cae3436bde86269b654faa4
Paulo Cavalcante <pauloc1807@hotmail.com>
2025-02-23 09:25:07 -0300

    Adicionando pasta de novo jogo para versionamento

---
+++ desafio-amigo-secreto/app.js
@@ -0,0 +1,15 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
+let amigos = [];
+
+function telaInicial(){
+    console.log("Jogo Inicializado")
+}
+function adicionarAmigo(){
+    amigos = document.querySelector("input").value;
+    console.log("O botão adicionar foi clicado!!!");
+    console.log(amigos);
+}
+function sortearAmigo(){
+
+}
+telaInicial();
