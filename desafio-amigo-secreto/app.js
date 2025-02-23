0799f26b5589eff79c1226f99d5bc1fd284f647c
Paulo Cavalcante <pauloc1807@hotmail.com>
2025-02-23 10:12:37 -0300

    Primeira versão funcional que adiciona nomes de amigos, com tarefas dos primeiros cards

--- desafio-amigo-secreto/app.js
+++ desafio-amigo-secreto/app.js
@@ -1,5 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
 let amigos = [];
+let listaAmigos = [];
 
 function telaInicial(){
     console.log("Jogo Inicializado")
@@ -7,9 +8,21 @@ function telaInicial(){
 function adicionarAmigo(){
     amigos = document.querySelector("input").value;
     console.log("O botão adicionar foi clicado!!!");
+    
+    if(amigos==""){
+    alert('Por favor, insira um nome.');
+    return;
+}
+    listaAmigos.push(amigos);
     console.log(amigos);
+    console.log(listaAmigos);
+    limparInput();
 }
 function sortearAmigo(){
 
+}
+function limparInput(){
+    amigos = document.querySelector("input");
+    amigos.value = "";
 }
 telaInicial();
