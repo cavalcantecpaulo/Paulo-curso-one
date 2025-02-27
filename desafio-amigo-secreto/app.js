d920013ba93b88bfeec9815ff531c5091bfd5057
Paulo Cavalcante <pauloc1807@hotmail.com>
2025-02-27 01:57:54 -0300

    Implementei função que está sorteando nomes e mostrando no console aleatoriamente, faltam ajustes finais para visualizar na tela

--- desafio-amigo-secreto/app.js
+++ desafio-amigo-secreto/app.js
@@ -1,28 +1,43 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
-let amigos = [];
+
 let listaAmigos = [];
 
-function telaInicial(){
-    console.log("Jogo Inicializado")
+function telaDoJogo(){
+    console.log("Jogo Inicializado");
 }
 function adicionarAmigo(){
-    amigos = document.querySelector("input").value;
-    console.log("O botão adicionar foi clicado!!!");
-    
-    if(amigos==""){
+    let amigo = document.querySelector("input").value;
+
+    if(amigo == ""){
     alert('Por favor, insira um nome.');
     return;
 }
-    listaAmigos.push(amigos);
-    console.log(amigos);
-    console.log(listaAmigos);
+    
+    listaAmigos.push(amigo);
+    console.log("Nome digitado:", amigo);
+    console.log("Amigos: ", listaAmigos);
+    atualizarLista();
     limparInput();
 }
+function atualizarLista(){
+    let array;
+    let i;
+    let li;
+    array = document.getElementsByClassName("name-list");
+    array.innerHTML = "";
+    for(i=0;i<listaAmigos.length; i++){
+        li = document.createElement("li");
+    }
+}
 function sortearAmigo(){
+    let numeroElementos = listaAmigos.length;
+    let indiceAleatorio = parseInt(Math.random() * numeroElementos);
+    console.log("Amigo sorteado: ", listaAmigos[indiceAleatorio]);
+}
 
-}
 function limparInput(){
-    amigos = document.querySelector("input");
-    amigos.value = "";
+    let amigo;
+    amigo = document.querySelector("input");
+    amigo.value = "";
 }
-telaInicial();
+telaDoJogo();
